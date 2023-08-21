5e08dae83b9146390e92d8eb9b656f5c8c4df625
BraindeadDogge <g.gulyuk@smsaero.ru>
2023-08-21 18:37:57 +0700

    fix

--- smsaero.js
+++ smsaero.js
@@ -1,250 +1,298 @@
-const axios = require('axios');
+const axios = require("axios");
 
 class SmsAero {
-  constructor(email, apiKey, urlGate = '@gate.smsaero.ru/v2/', signature = 'Sms Aero', typeSend = 2) {
-    this.email = email;
-    this.apiKey = apiKey;
-    this.urlGate = `https://${encodeURIComponent(email)}:${apiKey}${urlGate}`;
-    this.signature = signature;
-    this.typeSend = typeSend;
-    this.session = axios.create();
-  }
+	constructor(
+		email,
+		apiKey,
+		signature = "Sms Aero",
+		urlGate = "@gate.smsaero.ru/v2/",
+		typeSend = 2
+	) {
+		this.email = email;
+		this.apiKey = apiKey;
+		this.urlGate = `https://${encodeURIComponent(email)}:${apiKey}${urlGate}`;
+		this.signature = signature;
+		this.typeSend = typeSend;
+		this.session = axios.create();
+	}
 
-  async _request(selector, data = {}, page = null) {
-    const url = new URL(selector, this.urlGate);
-    if (page) {
-      url.searchParams.set('page', page);
-    }
-    try {
-      const response = await this.session.post(url.toString(), data);
-      return this._checkResponse(response.data);
-    } catch (err) {
-      throw new SmsAeroHTTPError(err);
-    }
-  }
+	async _request(selector, data = {}, page = null) {
+		const url = new URL(selector, this.urlGate);
+		if (page) {
+			url.searchParams.set("page", page);
+		}
+    
+		try {
+			const response = await this.session.post(url.toString(), data, {
+				auth: {
+					username: this.email,
+					password: this.apiKey,
+				},
+			});
+			return this._checkResponse(response.data);
+		} catch (err) {
+      console.log(err.response)
+			throw new SmsAeroHTTPError(err);
+		}
+	}
 
-  static _getNumber(number) {
-    if (Array.isArray(number)) {
-      return ['numbers', number];
-    } else {
-      return ['number', String(number)];
-    }
-  }
+	static _getNumber(number) {
+		if (Array.isArray(number)) {
+			return ["numbers", number];
+		} else {
+			return ["number", String(number)];
+		}
+	}
 
-  _checkResponse(response) {
-    if ('result' in response) {
-      if (response.result === 'reject') {
-        throw new SmsAeroError(response.reason);
-      } else if (response.result === 'no credits') {
-        throw new SmsAeroError(response.result);
-      }
-    }
-    return response;
-  }
+	_checkResponse(response) {
+		if ("result" in response) {
+			if (response.result === "reject") {
+				throw new SmsAeroError(response.reason);
+			} else if (response.result === "no credits") {
+				throw new SmsAeroError(response.result);
+			}
+		}
+		return response;
+	}
 
-  async send(number, text, dateSend = null, callbackUrl = null) {
-    const [num, phoneNumber] = SmsAero._getNumber(number);
-    const data = {
-      [num]: phoneNumber,
-      sign: this.signature,
-      text: text,
-      callbackUrl: callbackUrl
-    };
-    if (dateSend !== null) {
-      if (dateSend instanceof Date) {
-        data.dateSend = Math.floor(dateSend.getTime() / 1000);
-      } else {
-        throw new SmsAeroError('param `date` is not a Date object');
-      }
-    }
-    return this._request('sms/send', data);
-  }
+	async send(number, text, dateSend = null, callbackUrl = null) {
+		const [num, phoneNumber] = SmsAero._getNumber(number);
+		const data = {
+			[num]: phoneNumber,
+			sign: this.signature,
+			text: text,
+			callbackUrl: callbackUrl,
+		};
+		if (dateSend !== null) {
+			if (dateSend instanceof Date) {
+				data.dateSend = Math.floor(dateSend.getTime() / 1000);
+			} else {
+				throw new SmsAeroError("param `date` is not a Date object");
+			}
+		}
+		return this._request("sms/send", data);
+	}
 
-  smsStatus(smsId) {
-    return this._request('sms/status', { id: smsId });
-  }
+	smsStatus(smsId) {
+		return this._request("sms/status", {id: smsId});
+	}
 
-  smsList(number = null, text = null, page = null) {
-    const data = {};
-    if (number !== null) {
-      data.number = String(number);
-    }
-    if (text !== null) {
-      data.text = text;
-    }
-    return this._request('sms/list', data, page);
-  }
+	smsList(number = null, text = null, page = null) {
+		const data = {};
+		if (number !== null) {
+			data.number = String(number);
+		}
+		if (text !== null) {
+			data.text = text;
+		}
+		return this._request("sms/list", data, page);
+	}
 
-  balance() {
-    return this._request('balance');
-  }
+	balance() {
+		return this._request("balance");
+	}
 
-  auth() {
-    return this._request('auth');
-  }
+	auth() {
+		return this._request("auth");
+	}
 
-  cards() {
-    return this._request('cards');
-  }
+	cards() {
+		return this._request("cards");
+	}
 
-  addBalance(sum, cardId) {
-    return this._request('balance/add', { sum: sum, cardId: cardId });
-  }
+	addBalance(sum, cardId) {
+		return this._request("balance/add", {sum: sum, cardId: cardId});
+	}
 
-  tariffs() {
-    return this._request('tariffs');
-  }
+	tariffs() {
+		return this._request("tariffs");
+	}
 
-  signAdd(name) {
-    return this._request('sign/add', { name: name });
-  }
+	signAdd(name) {
+		return this._request("sign/add", {name: name});
+	}
 
-  signList(page = null) {
-    return this._request('sign/list', null, page);
-  }
+	signList(page = null) {
+		return this._request("sign/list", null, page);
+	}
 
-  groupAdd(name) {
-    return this._request('group/add', { name: name });
-  }
+	groupAdd(name) {
+		return this._request("group/add", {name: name});
+	}
 
-  groupDelete(groupId) {
-    return this._request('group/delete', { id: groupId });
-  }
+	groupDelete(groupId) {
+		return this._request("group/delete", {id: groupId});
+	}
 
-  groupList(page = null) {
-    return this._request('group/list', null, page);
-  }
+	groupList(page = null) {
+		return this._request("group/list", null, page);
+	}
 
-  contactAdd(number, groupId = null, birthday = null, sex = null, lname = null,
-             fname = null, sname = null, param1 = null, param2 = null, param3 = null) {
-    return this._request('contact/add', {
-      number: String(number),
-      groupId: groupId,
-      birthday: birthday,
-      sex: sex,
-      lname: lname,
-      fname: fname,
-      sname: sname,
-      param1: param1,
-      param2: param2,
-      param3: param3
-    });
-  }
+	contactAdd(
+		number,
+		groupId = null,
+		birthday = null,
+		sex = null,
+		lname = null,
+		fname = null,
+		sname = null,
+		param1 = null,
+		param2 = null,
+		param3 = null
+	) {
+		return this._request("contact/add", {
+			number: String(number),
+			groupId: groupId,
+			birthday: birthday,
+			sex: sex,
+			lname: lname,
+			fname: fname,
+			sname: sname,
+			param1: param1,
+			param2: param2,
+			param3: param3,
+		});
+	}
 
-  contactDelete(contactId) {
-    return this._request('contact/delete', { id: contactId });
-  }
+	contactDelete(contactId) {
+		return this._request("contact/delete", {id: contactId});
+	}
 
-  contactList(number = null, groupId = null, birthday = null, sex = null, operator = null,
-               lname = null, fname = null, sname = null, page = null) {
-    return this._request('contact/list', {
-      number: number !== null ? String(number) : null,
-      groupId: groupId,
-      birthday: birthday,
-      sex: sex,
-      operator: operator,
-      lname: lname,
-      fname: fname,
-      sname: sname
-    }, page);
-  }
+	contactList(
+		number = null,
+		groupId = null,
+		birthday = null,
+		sex = null,
+		operator = null,
+		lname = null,
+		fname = null,
+		sname = null,
+		page = null
+	) {
+		return this._request(
+			"contact/list",
+			{
+				number: number !== null ? String(number) : null,
+				groupId: groupId,
+				birthday: birthday,
+				sex: sex,
+				operator: operator,
+				lname: lname,
+				fname: fname,
+				sname: sname,
+			},
+			page
+		);
+	}
 
-  blacklistAdd(number) {
-    const [num, phoneNumber] = SmsAero._getNumber(number);
-    return this._request('blacklist/add', { [num]: phoneNumber });
-  }
+	blacklistAdd(number) {
+		const [num, phoneNumber] = SmsAero._getNumber(number);
+		return this._request("blacklist/add", {[num]: phoneNumber});
+	}
 
-  blacklistList(number = null, page = null) {
-    const data = number !== null ? { number: String(number) } : null;
-    return this._request('blacklist/list', data, page);
-  }
+	blacklistList(number = null, page = null) {
+		const data = number !== null ? {number: String(number)} : null;
+		return this._request("blacklist/list", data, page);
+	}
 
-  blacklistDelete(blacklistId) {
-    return this._request('blacklist/delete', { id: Number(blacklistId) });
-  }
+	blacklistDelete(blacklistId) {
+		return this._request("blacklist/delete", {id: Number(blacklistId)});
+	}
 
-  hlrCheck(number) {
-    const [num, phoneNumber] = SmsAero._getNumber(number);
-    return this._request('hlr/check', { [num]: phoneNumber });
-  }
+	hlrCheck(number) {
+		const [num, phoneNumber] = SmsAero._getNumber(number);
+		return this._request("hlr/check", {[num]: phoneNumber});
+	}
 
-  hlrStatus(hlrId) {
-    return this._request('hlr/status', { id: Number(hlrId) });
-  }
+	hlrStatus(hlrId) {
+		return this._request("hlr/status", {id: Number(hlrId)});
+	}
 
-  numberOperator(number) {
-    const [num, phoneNumber] = SmsAero._getNumber(number);
-    return this._request('number/operator', { [num]: phoneNumber });
-  }
+	numberOperator(number) {
+		const [num, phoneNumber] = SmsAero._getNumber(number);
+		return this._request("number/operator", {[num]: phoneNumber});
+	}
 
-  viberSend(sign, channel, text, number = null, groupId = null, imageSource = null,
-             textButton = null, linkButton = null, dateSend = null, signSms = null,
-             channelSms = null, textSms = null, priceSms = null) {
-    const [num, phoneNumber] = SmsAero._getNumber(number);
-    return this._request('viber/send', {
-      [num]: phoneNumber,
-      groupId: groupId !== null ? Number(groupId) : null,
-      sign: sign !== null ? String(sign) : null,
-      channel: channel !== null ? String(channel) : null,
-      text: text,
-      imageSource: imageSource,
-      textButton: textButton,
-      linkButton: linkButton,
-      dateSend: dateSend,
-      signSms: signSms,
-      channelSms: channelSms,
-      textSms: textSms,
-      priceSms: priceSms
-    });
-  }
+	viberSend(
+		sign,
+		channel,
+		text,
+		number = null,
+		groupId = null,
+		imageSource = null,
+		textButton = null,
+		linkButton = null,
+		dateSend = null,
+		signSms = null,
+		channelSms = null,
+		textSms = null,
+		priceSms = null
+	) {
+		const [num, phoneNumber] = SmsAero._getNumber(number);
+		return this._request("viber/send", {
+			[num]: phoneNumber,
+			groupId: groupId !== null ? Number(groupId) : null,
+			sign: sign !== null ? String(sign) : null,
+			channel: channel !== null ? String(channel) : null,
+			text: text,
+			imageSource: imageSource,
+			textButton: textButton,
+			linkButton: linkButton,
+			dateSend: dateSend,
+			signSms: signSms,
+			channelSms: channelSms,
+			textSms: textSms,
+			priceSms: priceSms,
+		});
+	}
 
-  viberSignList() {
-    return this._request('viber/sign/list');
-  }
+	viberSignList() {
+		return this._request("viber/sign/list");
+	}
 
-  viberList(page = null) {
-    return this._request('viber/list', null, page);
-  }
+	viberList(page = null) {
+		return this._request("viber/list", null, page);
+	}
 
-  flashcallSend(number, code) {
-    return this._request('flashcall/send', {
-      phone: number,
-      code: Number(code)
-    });
-  }
+	flashcallSend(number, code) {
+		return this._request("flashcall/send", {
+			phone: number,
+			code: Number(code),
+		});
+	}
 
-  flashcallList(number = null, text = null, page = null) {
-    const data = {};
-    if (number !== null) {
-      data.number = String(number);
-    }
-    if (text !== null) {
-      data.text = text;
-    }
-    return this._request('flashcall/list', data, page);
-  }
+	flashcallList(number = null, text = null, page = null) {
+		const data = {};
+		if (number !== null) {
+			data.number = String(number);
+		}
+		if (text !== null) {
+			data.text = text;
+		}
+		return this._request("flashcall/list", data, page);
+	}
 
-  flashcallStatus(pk) {
-    return this._request('flashcall/status', { id: pk });
-  }
+	flashcallStatus(pk) {
+		return this._request("flashcall/status", {id: pk});
+	}
 }
 
 class SmsAeroError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'SmsAeroError';
-  }
+	constructor(message) {
+		super(message);
+		this.name = "SmsAeroError";
+	}
 }
 
 class SmsAeroHTTPError extends SmsAeroError {
-  constructor(message) {
-    super(message);
-    this.name = 'SmsAeroHTTPError';
-  }
+	constructor(message) {
+		super(message);
+		this.name = "SmsAeroHTTPError";
+	}
 }
 
 module.exports = {
-  SmsAero: SmsAero,
-  SmsAeroError: SmsAeroError,
-  SmsAeroHTTPError: SmsAeroHTTPError
+	SmsAero: SmsAero,
+	SmsAeroError: SmsAeroError,
+	SmsAeroHTTPError: SmsAeroHTTPError,
 };
